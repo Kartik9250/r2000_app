5a4a5eed13bf4667db3bb0b58c713b8d4db1d9b5
Kartik9250 <kartikrana9250@gmail.com>
2023-01-09 17:45:44 +0530

    test2

--- pf_driver/scripts/final.py
+++ pf_driver/scripts/final.py
@@ -24,59 +24,18 @@ class lidar_plot:
 		plt.axis("equal")
 		# self.start()
 		plt.grid()
-
-	def _scan_registration(self, scan_topic):
-
-		self.angle_min = scan_topic.angle_min  # in radians
-		self.angle_max = scan_topic.angle_max  # in radians
-		self.angle_increment = scan_topic.angle_increment
-		self.range_min = scan_topic.range_min
-		self.range_min = scan_topic.range_max
-		self.range_arr = scan_topic.ranges  # array of range values
-		self.plotting_scan()
-		self.annot = ()
-		self.num_d = {}
-		self.ax = eval()
-		self.fig = eval()
-		# passing the range info to plotting scan
-
-	def plotting_scan(self):
-		x_arr = []
-		y_arr = []
-		dist= []
-		angle_min = self.angle_min
-		t = time.time()
-		for i in self.range_arr:
-			angle_min = angle_min + self.angle_increment
-			x = i * np.cos(angle_min)
-			y = i * np.sin(angle_min)
-			x_arr.append(x)
-			y_arr.append(y)
-
-		self.x_arr = x_arr
-		self.y_arr = y_arr
-		print(self.range_arr)
-		
-		self.num_d = {'Distance': self.range_arr}		# Dict shows the sales of flavor of each group 
-		self.fig,self.ax = plt.subplots()
-
-		self.annot = self.ax.annotate("", xy=(0,0), xytext=(10,10), textcoords='offset points', bbox=dict(boxstyle='round', fc='w'))
+		self.annot = self.ax.annotate("", xy=(0,0), xytext=(10,10), textcoords='offset points',
+					  bbox=dict(boxstyle='round', fc='w'))
 		self.annot.set_visible(False)
-
 		self.fig.canvas.mpl_connect("motion_notify_event", self.hover)
-
-
-	def update_annot(ind, self):
-		"""
-		This function updates the annotation.
-		:param ind:(dict)
-		"""
+	
+	def update_annot(self, ind):
 		pos = self.scatter.get_offsets()[ind["ind"][0]]
 		self.annot.xy = pos
 
 		k_lst = []
 		v_lst = []
-		print(self.num_d.items)
+
 		for k, v in self.num_d.items():
 			k_lst.append(k)
 			v_lst.append(v)
@@ -89,12 +48,11 @@ class lidar_plot:
 														
 		self.annot.set_text(text)
 
-
-	def hover(event, self):
+	def hover(self, event):
 		# bool
 		vis = self.annot.get_visible()
 		# if on the point, update the annot and show on the canvas
-		if event.inaxes == self.ax:
+		if self.event.inaxes == self.ax:
 			cont, ind = self.scatter.contains(event)
 			if cont:
 				self.update_annot(ind)
@@ -106,11 +64,35 @@ class lidar_plot:
 					self.annot.set_visible(False)
 					self.fig.canvas.draw_idle()
 
+	def _scan_registration(self, scan_topic):
 
-	
+		self.angle_min = scan_topic.angle_min  # in radians
+		self.angle_max = scan_topic.angle_max  # in radians
+		self.angle_increment = scan_topic.angle_increment
+		self.range_min = scan_topic.range_min
+		self.range_min = scan_topic.range_max
+		self.range_arr = scan_topic.ranges  # array of range values
+		self.plotting_scan()
+		# passing the range info to plotting scan
 
-			# print(x_arr)
-			# return x_arr, y_arr
+	def plotting_scan(self):
+		self.num_d = {'Distance':self.range_arr}
+		x_arr = []
+		y_arr = []
+		angle_min = self.angle_min
+		t = time.time()
+		for i in self.range_arr:
+			angle_min = angle_min + self.angle_increment
+			x = i * np.cos(angle_min)
+			y = i * np.sin(angle_min)
+			x_arr.append(x)
+			y_arr.append(y)
+
+		self.x_arr = x_arr
+		self.y_arr = y_arr
+
+		# print(x_arr)
+		# return x_arr, y_arr
 
 	def _update(self, i):
 		points = np.transpose(np.array([self.y_arr, self.x_arr]))
@@ -124,7 +106,7 @@ class lidar_plot:
 
 if __name__ == "__main__":
 	rospy.init_node("scan_node", anonymous=False)
-	scan_topic = "/scan"
+	scan_topic = "/laser/scan"
 	read = lidar_plot(scan_topic)
 	read.start()
 	# plt.axes("equal")
