64613078936ac56e08736327d11d77531a8075f2
NOEMOJI041 <aloksharma041@gmail.com>
2023-01-09 13:01:09 +0530

    laser data from ros with angle

---
+++ pf_driver/scripts/ldy.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+import rospy
+from sensor_msgs.msg import LaserScan
+import pandas as pd
+path = '/home/noemoji041/int_catkin/src/pf_lidar_ros_driver/pf_driver/data/Output.xlsm'
+df = pd.DataFrame()
+
+
+
+def callback(data):
+    list1 = list(data.ranges)
+    list2 = list(data.intensities)
+    df['ranges'] = list1
+    df['intensities'] = list2
+    list3 = []
+    for i in range(len(data.ranges)):
+        ray_angle = data.angle_min + (i * data.angle_increment)
+        list3.append(ray_angle)
+    df['ray_angle'] = list3
+    df.to_excel(path, index= False)
+
+    
+        
+
+
+
+def start():
+    rospy.init_node('LDdata')
+    rospy.Subscriber("/scan", LaserScan, callback)
+   
+
+
+
+
+    rospy.spin()
+
+
+
+
+
+start()
