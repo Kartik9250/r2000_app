b271f3ac926f6982bc21a3436ee46e73d9bc603f
Kartik9250 <kartikrana9250@gmail.com>
2023-01-09 17:21:35 +0530

    testing hover

--- pf_driver/scripts/final.py
+++ pf_driver/scripts/final.py
@@ -9,123 +9,125 @@ import time
 
 
 class lidar_plot:
-    def __init__(self, scan_topic):
-        # subscribing the scan topic
-        print(scan_topic)
-        self.scan_info = rospy.Subscriber(
-            scan_topic, LaserScan, self._scan_registration)
-        self.fig = plt.figure(figsize=(10,5))
-        self.ax = plt.axes()
-        self.ax.set_xlim(10, -15)
-        self.ax.set_ylim(8, -8)
-        self.scatter = self.ax.scatter([], [], s=2, color = "red")
-        self.x_arr = []
-        self.y_arr = []
-        plt.axis("equal")
-        # self.start()
-        plt.grid()
+	def __init__(self, scan_topic):
+		# subscribing the scan topic
+		print(scan_topic)
+		self.scan_info = rospy.Subscriber(
+			scan_topic, LaserScan, self._scan_registration)
+		self.fig = plt.figure(figsize=(10,5))
+		self.ax = plt.axes()
+		self.ax.set_xlim(10, -15)
+		self.ax.set_ylim(8, -8)
+		self.scatter = self.ax.scatter([], [], s=2, color = "red")
+		self.x_arr = []
+		self.y_arr = []
+		plt.axis("equal")
+		# self.start()
+		plt.grid()
 
-    def _scan_registration(self, scan_topic):
+	def _scan_registration(self, scan_topic):
 
-        self.angle_min = scan_topic.angle_min  # in radians
-        self.angle_max = scan_topic.angle_max  # in radians
-        self.angle_increment = scan_topic.angle_increment
-        self.range_min = scan_topic.range_min
-        self.range_min = scan_topic.range_max
-        self.range_arr = scan_topic.ranges  # array of range values
-        self.plotting_scan()
-        # passing the range info to plotting scan
+		self.angle_min = scan_topic.angle_min  # in radians
+		self.angle_max = scan_topic.angle_max  # in radians
+		self.angle_increment = scan_topic.angle_increment
+		self.range_min = scan_topic.range_min
+		self.range_min = scan_topic.range_max
+		self.range_arr = scan_topic.ranges  # array of range values
+		self.plotting_scan()
+		self.annot = ()
+		self.num_d = {}
+		self.ax = eval()
+		self.fig = eval()
+		# passing the range info to plotting scan
 
-    def plotting_scan(self):
-        x_arr = []
-        y_arr = []
-        angle_min = self.angle_min
-        t = time.time()
-        for i in self.range_arr:
-            angle_min = angle_min + self.angle_increment
-            x = i * np.cos(angle_min)
-            y = i * np.sin(angle_min)
-            x_arr.append(x)
-            y_arr.append(y)
+	def plotting_scan(self):
+		x_arr = []
+		y_arr = []
+		dist= []
+		angle_min = self.angle_min
+		t = time.time()
+		for i in self.range_arr:
+			angle_min = angle_min + self.angle_increment
+			x = i * np.cos(angle_min)
+			y = i * np.sin(angle_min)
+			x_arr.append(x)
+			y_arr.append(y)
 
-        self.x_arr = x_arr
-        self.y_arr = y_arr
+		self.x_arr = x_arr
+		self.y_arr = y_arr
+		print(self.range_arr)
+		
+		self.num_d = {'Distance': self.range_arr}		# Dict shows the sales of flavor of each group 
+		self.fig,self.ax = plt.subplots()
 
-        # print(x_arr)
-        # return x_arr, y_arr
+		self.annot = self.ax.annotate("", xy=(0,0), xytext=(10,10), textcoords='offset points', bbox=dict(boxstyle='round', fc='w'))
+		self.annot.set_visible(False)
 
-    def _update(self, i):
-        points = np.transpose(np.array([self.y_arr, self.x_arr]))
-        self.scatter.set_offsets(points)
-        return self.scatter,
+		self.fig.canvas.mpl_connect("motion_notify_event", self.hover)
 
-    def start(self):
-        self.anim = FuncAnimation(
-            self.fig, self._update, interval=20, blit=True)
+
+	def update_annot(ind, self):
+		"""
+		This function updates the annotation.
+		:param ind:(dict)
+		"""
+		pos = self.scatter.get_offsets()[ind["ind"][0]]
+		self.annot.xy = pos
+
+		k_lst = []
+		v_lst = []
+		print(self.num_d.items)
+		for k, v in self.num_d.items():
+			k_lst.append(k)
+			v_lst.append(v)
+		print(k_lst, v_lst)
+		ans_lst = []
+		for line in v_lst:
+			ans_lst.append(line[int(ind['ind'])])
+		
+		text = f"{k_lst[0]}:{ans_lst[0]}"
+														
+		self.annot.set_text(text)
+
+
+	def hover(event, self):
+		# bool
+		vis = self.annot.get_visible()
+		# if on the point, update the annot and show on the canvas
+		if event.inaxes == self.ax:
+			cont, ind = self.scatter.contains(event)
+			if cont:
+				self.update_annot(ind)
+				self.annot.set_visible(True)
+				self.fig.canvas.draw_idle()
+			else:
+				# not on the point, so will not show any annot
+				if vis:
+					self.annot.set_visible(False)
+					self.fig.canvas.draw_idle()
+
+
+	
+
+			# print(x_arr)
+			# return x_arr, y_arr
+
+	def _update(self, i):
+		points = np.transpose(np.array([self.y_arr, self.x_arr]))
+		self.scatter.set_offsets(points)
+		return self.scatter,
+
+	def start(self):
+		self.anim = FuncAnimation(
+			self.fig, self._update, interval=20, blit=True)
 
 
 if __name__ == "__main__":
-    rospy.init_node("scan_node", anonymous=False)
-    scan_topic = "/scan"
-    read = lidar_plot(scan_topic)
-    read.start()
-    # plt.axes("equal")
-    plt.show()
+	rospy.init_node("scan_node", anonymous=False)
+	scan_topic = "/scan"
+	read = lidar_plot(scan_topic)
+	read.start()
+	# plt.axes("equal")
+	plt.show()
 
-    rospy.spin()
-
-
-# def data_read(f):
-#     """
-#     Reading LIDAR laser beams (angles and corresponding distance data)
-#     """
-#     measures = [line.split(",") for line in open(f)]
-#     angles = []
-#     distances = []
-#     for measure in measures:
-#         angles.append(float(measure[0]))
-#         distances.append(float(measure[1]))
-#     angles = np.array(angles)
-#     distances = np.array(distances)
-
-#     ang, dist = data_read("lidar01.csv")
-#     ox = np.sin(ang) * dist
-#     oy = np.cos(ang) * dist
-
-#     return angles, distances
-
-
-# def listner():
-#     rospy.init_node("scan_node", anonymous=False)
-#     rospy.Subscriber("/scan", LaserScan, data_read)
-
-
-# plt.axis("equal")
-
-
-# fig = plt.figure()
-
-# # marking the x-axis and y-axis
-# axis = plt.axes(xlim=(-5, 5),
-#                 ylim=(-5, 5))
-
-# line, = axis.plot([], [], lw=3)
-
-
-# def init():
-#     line.set_data([], [])
-#     return line,
-
-
-# def animate(i):
-#     x = np.linspace(0, 4, 1000)
-
-#     # plots a sine graph
-#     y = np.sin(2 * np.pi * (x - 0.01 * i))
-#     line.set_data(x, y)
-
-#     return line,
-
-
-# anim = FuncAnimation(fig, animate, init_func=init,
-#                      frames=200, interval=20, blit=True)
+	rospy.spin()
