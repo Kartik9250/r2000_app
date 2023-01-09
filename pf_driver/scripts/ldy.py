4e44e71b617257a4d969e0374787b3ce8508f4ab
NOEMOJI041 <aloksharma041@gmail.com>
2023-01-09 16:53:38 +0530

    lidar viz added

--- pf_driver/scripts/ldy.py
+++ pf_driver/scripts/ldy.py
@@ -3,19 +3,20 @@
 import rospy
 from sensor_msgs.msg import LaserScan
 import pandas as pd
-path = '/home/noemoji041/int_catkin/src/pf_lidar_ros_driver/pf_driver/data/ok.csv'
+path = '/home/noemoji041/int_catkin/src/pf_lidar_ros_driver/ok.csv'
 df = pd.DataFrame()
 
 
 
 def callback(data):
+    print(data)
     list1 = list(data.ranges)
     list2 = list(data.intensities)
     df['ranges'] = list1
     df['intensities'] = list2
     list3 = []
     for i in range(len(data.ranges)):
-        ray_angle = (data.angle_min + (i * data.angle_increment))*57.2957795131
+        ray_angle = (data.angle_min + (i * data.angle_increment))
         list3.append(ray_angle)
     df['ray_angle'] = list3
     df.to_csv(path, index= False)
@@ -26,8 +27,13 @@ def callback(data):
 
 
 def start():
+
     rospy.init_node('LDdata')
+    rate = rospy.Rate(0.000001)
+
     rospy.Subscriber("/scan", LaserScan, callback)
+
+    rate.sleep()
    
 
 
--- plot_data.py
+++ plot_data.py
@@ -1,8 +1,15 @@
 import math
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 from math import cos, sin, radians, pi
-import lidar_to_grid as lg
+from matplotlib.animation import FuncAnimation
+from itertools import count
+
+
+plt.style.use('fivethirtyeight')
+
+
 
 def file_read(f):
     """
@@ -12,40 +19,47 @@ def file_read(f):
     angles = []
     distances = []
     for measure in measures:
-        try:
-            angles.append(float(measure[2][0:-1]))
-            distances.append(float(measure[0]))
-        except:
-            pass
+            try:
+                angles.append(float(measure[2][0:-1]))
+                distances.append(float(measure[0]))
+            except:
+                pass
     angles = np.array(angles)
     distances = np.array(distances)
     return angles, distances
 #print(file_read("ok.csv"))
 
-ang, dist = file_read("ok.csv")
-ox = np.sin(ang) * dist
-oy = np.cos(ang) * dist
-plt.figure(figsize=(10,5))
-plt.scatter([ox], [oy], color="red", s= 2) # lines from 0,0 to the
-plt.axis("equal")
-bottom, top = plt.ylim()  # return the current ylim
-plt.ylim((top, bottom)) # rescale y axis, to match the grid orientation
-plt.grid(True)
-plt.show()
+# ang, dist = file_read("ok.csv")
+# ox = np.sin(ang) * dist
+# oy = np.cos(ang) * dist
+# plt.figure(figsize=(10,5))
+# plt.scatter([ox], [oy], color="red", s= 2) # lines from 0,0 to the
+# plt.axis("equal")
+# bottom, top = plt.ylim()  # return the current ylim
+# plt.ylim((top, bottom)) # rescale y axis, to match the grid orientation
+# plt.grid(True)
 
-xyreso = 0.02  # x-y grid resolution
-yawreso = math.radians(3.1)  # yaw angle resolution [rad]
-ang, dist = file_read("ok.csv")
-ox = np.sin(ang) * dist
-oy = np.cos(ang) * dist
-pmap, minx, maxx, miny, maxy, xyreso = lg.generate_ray_casting_grid_map(ox, oy, xyreso, False)
-xyres = np.array(pmap).shape
-plt.figure(figsize=(20,8))
-#plt.subplot(122)
-plt.imshow(pmap, cmap = "PiYG_r")
-plt.clim(-0.4, 1.4)
-plt.gca().set_xticks(np.arange(-.5, xyres[1], 1), minor = True)
-plt.gca().set_yticks(np.arange(-.5, xyres[0], 1), minor = True)
-plt.grid(True, which="minor", color="w", linewidth = .6, alpha = 0.5)
-plt.colorbar()
+index = count()
+
+
+def animate(i):
+    ang, dist = file_read("ok.csv")
+    ox = np.sin(ang) * dist
+    oy = np.cos(ang) * dist    
+    plt.cla()
+    plt.scatter([ox], [oy], color="red", s= 2) # lines from 0,0 to the
+    # plt.axis("equal")
+    # bottom, top = plt.ylim()  
+    # return the current ylim
+    plt.ylim((6, -6))
+    
+    # rescale y axis, to match the grid orientation
+
+    # plt.gcf().autofmt_xdate()
+    # plt.tight_layout()
+
+ani = FuncAnimation(plt.figure(), animate, 10)
+
+
+# plt.tight_layout()
 plt.show()
